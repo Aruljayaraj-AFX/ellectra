7f403b8f9c66bcfb2076c13e476a69c0bc2ac384
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-11-04 23:19:08 +0530

    ch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Profile from "./components/Profile";
 import Test from "./components/test";
 import Cartoper from "./components/cartcrud";
 import Proadmin from "./components/product-admin"
-import visting-card from "./components/visting-card"
+import vistingcard from "./components/visting-card"
 
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
           <Route path="/admin-testforupload" element={<Test />} />
           <Route path="/admin-catogeries" element={<Cartoper />} />
           <Route path="/admin-pro" element={<Proadmin />} />
-          <Route path="/welcome-dude/visting-via/qr-code" element={<visiting-card/>}/>
+          <Route path="/welcome-dude/visting-via/qr-code" element={<vistingcard/>}/>
         </Route>
       </Routes>
     </Suspense>
