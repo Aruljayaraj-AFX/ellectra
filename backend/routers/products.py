779942c417951f688bbab72230821f645886d23d
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-11-04 17:38:21 +0530

    change

--- backend/routers/products.py
+++ backend/routers/products.py
@@ -15,13 +15,9 @@ async def get_pag_cat_handle(db=Depends(get_DB)):
 async def get_pro_handle(pagination:int,db=Depends(get_DB)):
     return await get_cat_detail(pagination,db)
 
-@router_product.get("/pag_pro_info")
-async def get_pag_pro_handle(db=Depends(get_DB),categories_id: str = Query(..., alias="catgories_id")):
-    return await get_pro_pag(db,categories_id)
-
 @router_product.get("/pro_info")
-async def get_cat_handle(pagination:int,catgories_id:str,db=Depends(get_DB)):
-    return await get_pro_detail(pagination,catgories_id,db)
+async def get_cat_handle(catgories_id:str,db=Depends(get_DB)):
+    return await get_pro_detail(catgories_id,db)
 
 @router_product.get("all_cart")
 async def get_all_cat_name(db=Depends(get_DB)):
