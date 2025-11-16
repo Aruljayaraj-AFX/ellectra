37cb930baab374da50d69288616423a49e4c9a99
Arul Jayaraj <aruljayarajj826@gmail.com>
2025-11-16 16:44:05 +0530

    vc

--- frontend/src/components/visitingcard.jsx
+++ frontend/src/components/visitingcard.jsx
@@ -6,6 +6,17 @@ import Foot from "../assets/foot.png";
 import MO from "../assets/mo.png";
 
 export default function EnhancedEllectraPage() {
+  const handleClick = () => {
+    const text = encodeURIComponent('Hello! I am contacting you from your website.');
+    const url = `https://wa.me/${+916381733447}?text=${text}`;
+    window.open(url, '_blank', 'noopener');
+  };
+  const handleClickInt = () => {
+    window.open("https://www.instagram.com/ellectra_projects/?igsh=MWRqanpycngzazY5cw%3D%3D#", "_blank", "noopener");
+  };
+  const handleClickWeb = () => {
+    window.open("https://www.ellectra.in/", "_blank", "noopener");
+  };
   return (
     <div className="min-h-screen bg-[#EBEBEB]">
       <div className="flex justify-between items-center p-4 sm:p-6 md:p-10">
@@ -27,7 +38,7 @@ export default function EnhancedEllectraPage() {
   <img src={MO}  alt="" />
 </div>
 
-<div className="flex md:hidden lg:hidden sm:flex border rounded-3xl border-gray-300 shadow-md px-4 w-full  lg:w-2/3 justify-between  mb-10 p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
+<div onClick={handleClickWeb} className="flex md:hidden lg:hidden sm:flex border rounded-3xl border-gray-300 shadow-md px-4 w-full  lg:w-2/3 justify-between  mb-10 p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
   <p style={{ fontFamily: "Bai Jamjuree,sans-serif" }} className="text-lg sm:text-xl md:text-xl lg:text-2xl text-gray-400 truncate">
     https://www.ellectra.in
   </p>
@@ -59,14 +70,14 @@ export default function EnhancedEllectraPage() {
         <div className="flex flex-col justify-between w-full gap-4 sm:gap-5">
 
           <div className="flex  xs:flex-row gap-3 sm:gap-5 justify-center lg:justify-end mt-4 sm:mt-6 md:mt-10">
-            <div className="border rounded-3xl border-gray-300 shadow-md px-4 sm:px-5 flex gap-2 sm:gap-3 p-2 sm:p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
+            <div onClick={handleClick} className="border rounded-3xl border-gray-300 shadow-md px-4 sm:px-5 flex gap-2 sm:gap-3 p-2 sm:p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
               <img src={whatsapp} className="w-8 h-8 sm:w-10 sm:h-10" alt="WhatsApp" />
               <p style={{ fontFamily: "Bai Jamjuree,sans-serif" }} className="text-md sm:text-md md:text-2xl">
                 Whatsapp
               </p>
             </div>
 
-            <div className="border rounded-3xl border-gray-300 shadow-md px-4 sm:px-5 flex gap-2 sm:gap-3 p-2 sm:p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
+            <div onClick={handleClickInt} className="border rounded-3xl border-gray-300 shadow-md px-4 sm:px-5 flex gap-2 sm:gap-3 p-2 sm:p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
               <img src={Instagram} className="w-8 h-8 sm:w-10 sm:h-10" alt="Instagram" />
               <p style={{ fontFamily: "Bai Jamjuree,sans-serif" }} className="text-md sm:text-md md:text-2xl">
                 Instagram
@@ -74,7 +85,7 @@ export default function EnhancedEllectraPage() {
             </div>
           </div>
 
-          <div className="hidden md:flex lg:flex justify-center lg:justify-end">
+          <div onClick={handleClickWeb} className="z-100 hidden md:flex lg:flex justify-center lg:justify-end">
             <div className="border rounded-3xl border-gray-300 shadow-md px-4 sm:px-5 w-full lg:w-2/3 flex justify-between gap-2 sm:gap-3 p-2 sm:p-3 items-center cursor-pointer hover:shadow-lg transition-shadow">
               <p style={{ fontFamily: "Bai Jamjuree,sans-serif" }} className="text-sm sm:text-lg md:text-xl lg:text-2xl text-gray-400 truncate">
                 https://www.ellectra.in
